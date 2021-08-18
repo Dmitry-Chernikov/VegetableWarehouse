8f737ff894bdfc889b0888fa8b1786949a444c03
Dmitry <42750167+Dmitry-Chernikov@users.noreply.github.com>
2021-08-18 13:37:27 +0400

    Update Production.java

--- src/main/java/ru/dmitry/VegetableWarehouse/model/Production.java
+++ src/main/java/ru/dmitry/VegetableWarehouse/model/Production.java
@@ -1,53 +1,79 @@
 package ru.dmitry.VegetableWarehouse.model;
 
+import lombok.*;
+import javax.persistence.*;
+
 /**
  * Продукты
  */
 
-public enum Production {
-
+@Entity
+@Table(name = "PRODUCTION")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Production extends BaseEntity{
+            private static final long serialVersionUID = 1353555821748941084L;
+    
+     /**
+     * Наименование продукта
+     */
+    @Column(name = "Name_Production")
+    private String nameProduction; 
     /**
      * Клубниплоды
+     * Studes,
      */
-    Studes,
+    
     /**
      * Корнеплоды
+     * Roots,
      */
-    Roots,
+    
     /**
      * Капуста
+     * Cabbage,
      */
-    Cabbage,
+    
     /**
      * Салаты
+     * Salad,
      */
-    Salad,
+    
     /**
      * Пряные
+     * Spicy,
      */
-    Spicy,
+    
     /**
      * Луковые
+     * Bulbous,
      */
-    Bulbous,
+    
     /**
      * Паслёновые
+     * Parenic,
      */
-    Parenic,
+    
     /**
      * Бахчевые
+     * Bakhchy,
      */
-    Bakhchy,
+    
     /**
      * Бобовые
+     * Beans,
      */
-    Beans,
+    
     /**
      * Зерновые
+     * Grain,
      */
-    Grain,
+    
     /**
      * Десертные
+     * Dessert
      */
-    Dessert
+    
 }
