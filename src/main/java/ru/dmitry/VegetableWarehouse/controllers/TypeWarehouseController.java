e5d5f949a9aa93477193b5c0d5f4fb9346a2fb18
Dmitry <42750167+Dmitry-Chernikov@users.noreply.github.com>
2021-10-05 09:52:12 +0400

    Update TypeWarehouseController.java

--- src/main/java/ru/dmitry/VegetableWarehouse/controllers/TypeWarehouseController.java
+++ src/main/java/ru/dmitry/VegetableWarehouse/controllers/TypeWarehouseController.java
@@ -19,27 +19,32 @@ public class TypeWarehouseController {
 
     private final TypeWarehouseService typeWarehouseService;
 
+        @ApiOperation("Метод возвращает все имеющися склады овощебазы которые храняться в таблице \"Типы складов\"")
     @GetMapping(path = "/typeWarehouse")
     public List<TypeWarehouseDto> getAllTypeWarehouse() {
         return typeWarehouseService.findAll();
     }
 
+         @ApiOperation("Метод возвращает тип склада овощебазы по id из таблицы \"Типы складов\"")
     @GetMapping(path = "/typeWarehouse/{id}")
     public ResponseEntity<TypeWarehouseDto> getTypeWarehouseById(@PathVariable("id") Long id) {
         Optional<TypeWarehouseDto> typeWarehouseDto = Optional.ofNullable(typeWarehouseService.findById(id));
         return typeWarehouseDto.map(warehouseDto -> new ResponseEntity<>(warehouseDto, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
+        @ApiOperation("Метод добовляет тип склада в таблицу \"Типы складов\"")
     @PostMapping(path = "/typeWarehouse")
     public TypeWarehouseDto createTypeWarehouse(@RequestBody TypeWarehouseDto typeWarehouseDto) {
         return typeWarehouseService.save(typeWarehouseDto);
     }
 
+        @ApiOperation("Метод изменяет атрибуты типа склада в таблице \"Типы складов\"")
     @PutMapping(path = "/typeWarehouse/{id}")
     public TypeWarehouseDto updateTypeWarehouse(@RequestBody TypeWarehouseDto typeWarehouseDto) {
         return typeWarehouseService.save(typeWarehouseDto);
     }
-
+    
+        @ApiOperation("Метод удаляет из таблицы \"Типы складов\" по id имеющийся склад")
     @DeleteMapping(path = "/typeWarehouse/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteTypeWarehouse(@PathVariable("id") Long id) {
