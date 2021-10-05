11c715eb9ab3147c9bc4d20739469aea35e9eec5
Dmitry <42750167+Dmitry-Chernikov@users.noreply.github.com>
2021-10-05 09:02:09 +0400

    Update ClientsController.java

--- src/main/java/ru/dmitry/VegetableWarehouse/controllers/ClientsController.java
+++ src/main/java/ru/dmitry/VegetableWarehouse/controllers/ClientsController.java
@@ -17,28 +17,28 @@ import java.util.Optional;
 public class ClientsController {
 
     private final ClientsService clientsService;
-
+        @ApiOperation("Метод возвращает всех клиентов овощебазы")
     @GetMapping(path = "/clients")
     public Iterable<ClientsDto> getAllClients() {
         return clientsService.findAll();
     }
-
+        @ApiOperation("Метод возвращает одного клиента овощебазы по id")
     @GetMapping(path = "/clients/{id}")
     public ResponseEntity<ClientsDto> getClientsById(@PathVariable("id") Long id) {
         Optional<ClientsDto> clientsDto = Optional.ofNullable(clientsService.findById(id));
         return clientsDto.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
-
+        @ApiOperation("Метод создаёт в таблице Клиенты одного клиента")
     @PostMapping(path = "/clients")
     public ClientsDto createClients(@RequestBody ClientsDto clientsDto) {
         return clientsService.save(clientsDto);
     }
-
+        @ApiOperation("Метод изменяет атрибуты клиента в таблице Клиенты")
     @PutMapping(path = "/clients/{id}")
     public ClientsDto updateClients(@RequestBody ClientsDto clientsDto) {
         return clientsService.save(clientsDto);
     }
-
+        @ApiOperation("Метод удаляет клиента из таблицы Клиенты по id")
     @DeleteMapping(path = "/clients/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteClients(@PathVariable("id") Long id) {
