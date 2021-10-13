8242cb5c016dc2e6830bb3989749bc182cffe2c6
Dmitry-Chernikov <chernicov_dima@mail.ru>
2021-10-13 21:55:54 +0400

    дополнил тест UnitsServiceTest

--- src/test/java/ru/dmitry/VegetableWarehouse/services/UnitsServiceTest.java
+++ src/test/java/ru/dmitry/VegetableWarehouse/services/UnitsServiceTest.java
@@ -1,13 +1,11 @@
 package ru.dmitry.VegetableWarehouse.services;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.dmitry.VegetableWarehouse.dto.UnitsDto;
 import ru.dmitry.VegetableWarehouse.mappers.UnitsMapper;
 import ru.dmitry.VegetableWarehouse.model.Units;
@@ -16,27 +14,30 @@ import ru.dmitry.VegetableWarehouse.repositories.UnitsRepository;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 //@RunWith(MockitoJUnitRunner.class)
-//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 //@RunWith(SpringRunner.class)
 //@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class UnitsServiceTest {
-
-    @Mock //создаёт фиктивную реализацию
+    //@Mock
+    @MockBean //создаёт фиктивную реализацию
     private UnitsMapper mapper;
 
-    @Mock
+    //@Mock
+    @MockBean
     private UnitsRepository unitsRepository;
 
-    @InjectMocks //создаёт макет реализации, дополнительно вводит в него фиктивные реализации
-    private UnitsService unitsService =  new UnitsService(unitsRepository, mapper);
+    //@InjectMocks //создаёт макет реализации, дополнительно вводит в него фиктивные реализации
+    @Autowired
+    private UnitsService unitsService;
 
-    @BeforeEach // Выполнять пред каждым
+   @BeforeEach // Выполнять пред каждым
     public void init() {
+        //MockitoAnnotations.initMocks(this);
         List<Units> unitsList = new ArrayList<Units>();//unitsRepository.findAll();
         Units unitsOne = new Units();
         Units unitsTwo = new Units();
@@ -50,48 +51,56 @@ class UnitsServiceTest {
         unitsList.add(unitsOne);
         unitsList.add(unitsTwo);
         when(unitsRepository.findAll()).thenReturn(unitsList);
+
+        //unitsService = new UnitsService(this.unitsRepository,this.mapper);
+
     }
 
 
-    @DisplayName("Тест Mock findAll")
+    @DisplayName("Тест findAllTest")
     @Test
-    public void findAll() {
-        List<UnitsDto> unitsDto = unitsService.findAll();
-        assertEquals(2, unitsDto.size());
-        verify(unitsRepository, times(1)).findAll();
+    public void findAllTest() {
+        List<UnitsDto> unitsDtoList = unitsService.findAll();
+        assertEquals(unitsRepository.findAll().size(), unitsDtoList.size());
+    }
+
+    @DisplayName("Тест saveTest")
+    @Test
+    public void saveTest() {
+        UnitsDto unitsDto = new UnitsDto();
+        unitsDto.setId(1L);
+        unitsDto.setMeasurementUnit("Евро");
+        unitsDto.setDesignationUnit("EUR");
+
+        unitsService.save(unitsDto);
+        Units units = unitsRepository.findById(1L).orElse(null);
+
+        assertEquals(units.getDesignationUnit(),unitsDto.getDesignationUnit());
+    }
+
+
+    @DisplayName("Тест updateTest")
+    @Test
+    void updateTest() {
+        UnitsDto unitsDto = unitsService.findById(1L);
+        unitsDto.setDesignationUnit("тест");
+        assertEquals(unitsRepository.findById(1L).get().getDesignationUnit(),unitsDto.getDesignationUnit());
+    }
+
+
+    @DisplayName("Тест finByIdTest")
+    @Test
+    void findByIdTest() {
+        UnitsDto unitsDto = unitsService.findById(1L);
+        assertEquals(unitsDto.getId().longValue(),1L);
+    }
+
+
+    @DisplayName("Тест deleteByIdTest")
+    @Test
+    void deleteByIdTest() {
+        unitsService.deleteById(1L);
+        unitsService.deleteById(2L);
+        assertEquals(unitsRepository.findAll().size(), 0);
     }
 }
- /*   @Test
-    public void save() throws Exception {
-        UnitsDto unitsDto = new UnitsDto();
-        unitsDto.setId(11L);
-        unitsDto.setMeasurementUnit("Тонна");
-        unitsDto.setDesignationUnit("т/t");
-
-        this.unitsRepository.save(this.mapper.toEntity(unitsDto));
-        //unitsDto = unitsService.findById(11L);
-        Assertions.assertThat(unitsDto.getId()).isGreaterThan(0L);
-    }
-
-    @Test
-    void findById() {
-        Units units = unitsRepository.findById(10L).get();
-        Assertions.assertThat(units.getId()).isEqualTo(10L);
-    }
-
-    @Test
-    void update() {
-        Units units = unitsRepository.findById(10L).get();
-        units.setDesignationUnit("тест");
-        Assertions.assertThat((unitsRepository.save(units)).getDesignationUnit()).isEqualTo("тест");
-    }
-
-    @Test
-    void deleteById() {
-        UnitsDto unitsDto = new UnitsDto();
-        unitsDto = this.unitsService.findById(2L);
-        Mockito.verify(unitsRepository).findById(2l);
-
-        Assertions.assertThat(unitsDto.getDesignationUnit()).isEqualTo("ц");
-        this.unitsService.deleteById(10L);
-    }*/
